595d798634696c2472dd4a8d5567e4f9967788f1
Young Sam <iamyoungsam7@gmail.com>
2025-06-11 06:29:57 +0100

    Learnt how to use the react-icons, how to run dev and why I need to

--- src/components/FooterDefault.jsx
+++ src/components/FooterDefault.jsx
@@ -1,9 +1,10 @@
 import React from 'react'
+import { FaCopyright } from 'react-icons/fa'
 
 const FooterDefault = () => {
   return (
     <footer class="bg-gray-800 text-gray-300 text-center py-6">
-      <p>&copy; 2025 SmartWeb Agency. All rights reserved.</p>
+      <p>{ FaCopyright } 2025 SmartWeb Agency. All rights reserved.</p>
   </footer>
   )
 }
